8f515bccb9f883f1cc1ea1fb1cfeff5565e9954a
Juan Carlos <juan.castro.socla@gmail.com>
2022-08-30 00:14:31 -0500

    Additional Resources

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -27,13 +27,29 @@ namespace Service
 
             var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
             if (employeesFromDb is null)
-                throw new EmployeesNotFoundException(companyId);
+                throw new EmployeeNotFoundException(companyId);
 
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
 
             return employeesDto;
 
         }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+            if (employeeDb is null)
+                throw new EmployeeNotFoundException(id);
+
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return employee;
+
+        }
+
 
     }
 }
