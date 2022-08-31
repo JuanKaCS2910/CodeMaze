bed7555c9919e11132c3d46d0f56dfcb84c6bb76
Juan Carlos <juan.castro.socla@gmail.com>
2022-08-30 23:24:01 -0500

    Response XML

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -14,8 +14,11 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers()
-       .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+}).AddXmlDataContractSerializerFormatters()
+.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
